a628ffbe4d17e5d298f5940f3d1478c298696fd2
Serova Alina <serovaallina2005@gmail.com>
2025-10-03 02:27:51 +0300

    d

--- src/Components/UsersToolbar.jsx
+++ src/Components/UsersToolbar.jsx
@@ -1,52 +1,61 @@
 import React from 'react';
 import { EnhancedTableToolbar } from '../Components/EnhancedTableToolbar';
-import { useBlockUsersMutation, useUnblockUsersMutation, useDeleteUsersMutation, useDeleteUnverifyUsersMutation } from '../services/usersApi';
-
+import {
+  useBlockUsersMutation,
+  useUnblockUsersMutation,
+  useDeleteUsersMutation,
+  useDeleteUnverifyUsersMutation,
+} from '../services/usersApi';
+import { useNavigate } from 'react-router-dom';
 const UsersToolbar = ({ selectedUsers, onSelectionClear, onFilterChange }) => {
   const [blockUsers] = useBlockUsersMutation();
   const [unblockUsers] = useUnblockUsersMutation();
   const [deleteUsers] = useDeleteUsersMutation();
   const [deleteUnverifyUsers] = useDeleteUnverifyUsersMutation();
-
+  const navigate = useNavigate();
   const handleBlockUsers = async (userIds) => {
     try {
       await blockUsers(userIds).unwrap();
       onSelectionClear();
     } catch (error) {
       console.log('Failed to block users:', error);
-      throw error;
+      if (error.status == 500 && error.data.errormessages[0].includes('User not found'))
+        navigate('/login?error=Your account is delete');
     }
   };
-  
-    const handleUnblockUsers = async (userIds) => {
-      try {
-        await unblockUsers(userIds).unwrap();
-        onSelectionClear();
-      } catch (error) {
-        console.log('Failed to unblock users:', error);
-        throw error;
-      }
-    };
-  
-    const handleDeleteUsers = async (userIds) => {
-      try {
-       await deleteUsers(userIds).unwrap();
-       onSelectionClear();
-      } catch (error) {
-        console.log('Failed to delete users:', error);
-        throw error;
-      }
-    };
 
-    const handleDeleteUnverifyUsers = async () => {
-      try {
-       await deleteUnverifyUsers().unwrap();
-      } catch (error) {
-        console.log('Failed to delete users:', error);
-        throw error;
-      }
-    };
-    
+  const handleUnblockUsers = async (userIds) => {
+    try {
+      await unblockUsers(userIds).unwrap();
+      onSelectionClear();
+    } catch (error) {
+      console.log('Failed to unblock users:', error);
+      if (error.status == 500 && error.data.errormessages[0].includes('User not found'))
+        navigate('/login?error=Your account is delete');
+    }
+  };
+
+  const handleDeleteUsers = async (userIds) => {
+    try {
+      await deleteUsers(userIds).unwrap();
+      onSelectionClear();
+    } catch (error) {
+      console.log('Failed to delete users:', error);
+      if (error.status == 500 && error.data.errormessages[0].includes('User not found'))
+        navigate('/login?error=Your account is delete');
+    }
+  };
+
+  const handleDeleteUnverifyUsers = async () => {
+    try {
+      await deleteUnverifyUsers().unwrap();
+    } catch (error) {
+      console.log('Failed to delete users:', error);
+      if (error.status == 500 && error.data.errormessages[0].includes('User not found'))
+        navigate('/login?error=Your account is delete');
+    }
+  };
+
   return (
     <EnhancedTableToolbar
       numSelected={selectedUsers.length}
