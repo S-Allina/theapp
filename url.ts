05c759ee069c3cb77d28b2c386b6d02a3763413e
Serova Alina <serovaallina2005@gmail.com>
2025-11-13 14:16:44 +0300

    fix url

--- url.ts
+++ url.ts
@@ -1,7 +1,10 @@
 const urls = {
-  FRONT: 'http://localhost:5173',
-  AUTH: 'https://localhost:7052',
-  MAIN: 'https://localhost:7004',
+  // FRONT: 'http://localhost:5173',
+  // AUTH: 'https://localhost:7052',
+  // MAIN: 'https://localhost:7004',
+  FRONT: 'https://s-allina.github.io',
+  AUTH: 'https://Identity.somee.com',
+  MAIN: 'https://CourseProject.somee.com',
 };
 
 export default urls;
